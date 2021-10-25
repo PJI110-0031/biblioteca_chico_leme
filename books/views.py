f49e84b02bc77bd778d3b7f2b539793bf0a0cf3e
Eduwardo Horibe <eduhoribe@gmail.com>
2021-10-25 18:17:14 -0300

    Limiting search to 3 characters

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import BadRequest
 from django.shortcuts import render
 
 from .models import Book
@@ -6,6 +7,9 @@ from .models import Book
 def index(request):
     if 'search' in request.GET and request.GET['search']:
         search_text = request.GET['search']
+
+        if len(search_text) < 3:
+            raise BadRequest("Search query must have at least 3 characters")
 
         result = Book.objects.filter(Book.search_query(search_text)).distinct()
 

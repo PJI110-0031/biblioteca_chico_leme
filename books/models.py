ffbacd334d4c2f3f710880e4571b11904f9c6212
Eduwardo Horibe <eduhoribe@gmail.com>
2021-11-17 21:17:06 -0300

    Heroku deploy

--- books/models.py
+++ books/models.py
@@ -132,7 +132,7 @@ class Book(models.Model):
     pha = models.CharField(max_length=50, blank=True, null=True, verbose_name=_('PHA'))
     shelf = models.ForeignKey(Shelf, on_delete=models.PROTECT, blank=True, null=True, verbose_name=_('Subject'))
     observations = models.TextField(max_length=2048, blank=True, null=True, verbose_name=_('Observations'))
-    status = models.CharField(max_length=1, choices=BookStatus.choices(), verbose_name=_('Status'), default=BookStatus.circulant)
+    status = models.CharField(max_length=255, choices=BookStatus.choices(), verbose_name=_('Status'), default=BookStatus.circulant)
 
     class Meta:
         verbose_name = _('Book')
